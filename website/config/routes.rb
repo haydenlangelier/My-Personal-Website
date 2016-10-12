12bb6e27afe06b57b7bf854a5134f89d8b159c0b
Hayden Langelier <haydenlangelier@utexas.edu>
2016-10-12 15:39:35 -0700

    routing to homepage is now functional

--- website/config/routes.rb
+++ website/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :friendships
   resources :interests
   resources :users
+
+  root 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
