9b29e7e0ff6e42515ac4b62e1e75aa17286eb15d
Hayden Langelier <haydenlangelier@utexas.edu>
2016-10-12 14:32:29 -0700

    starting anew to accomdate new design choices

--- personal_website/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :users
-  resources :events
-  get 'homepage/index'
-root 'homepage#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
